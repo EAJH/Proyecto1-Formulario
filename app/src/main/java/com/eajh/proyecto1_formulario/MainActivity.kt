36314b87dc116cc361e97b9618826b7959a9c42e
EAJH <erickadrianjh@gmail.com>
2026-03-31 17:47:57 -0600

    Adición foto perfil, título de sección 1 de formulario y limpieza en esqueleto de app. MainActivity limpio y Scaffold en MainScreen

--- app/src/main/java/com/eajh/proyecto1_formulario/MainActivity.kt
+++ app/src/main/java/com/eajh/proyecto1_formulario/MainActivity.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -17,16 +30,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.eajh.proyecto1_formulario.ui.theme.BackgroundApp
 import com.eajh.proyecto1_formulario.ui.theme.Black
+import com.eajh.proyecto1_formulario.ui.theme.DarkBlue
 import com.eajh.proyecto1_formulario.ui.theme.Proyecto1FormularioTheme
 import com.eajh.proyecto1_formulario.ui.theme.TopBarColor
 
@@ -41,14 +59,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Proyecto1FormularioTheme {
-                // Este Scaffold nos ayuda a obtener el padding necesario de la barra de
-                // estados de android y este se lo pasamos a MainScreen para que comience a
-                // dibujar la app partiendo debajo de esta barra.
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen(modifier = Modifier.fillMaxSize())
+
             }
         }
     }
@@ -59,7 +71,7 @@ fun MainScreen(modifier: Modifier = Modifier) {
     /*
     * El Scaffold es el esqueleto de la pantalla. Tiene como función organizar el espacio para
     * que los elementos comunes de una app no se amontonen. Poniendo un Scaffold aquí permite
-    * colocar la TopBar fácilmente.
+    * colocar la TopBar fácilmente y dejar el MainActivity más limpio.
     * */
     Scaffold(
         modifier = modifier, // Aplica el padding que viene del MainActivity para colocarnos
@@ -71,7 +83,13 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 content = "Creación de perfil",
                 onBackClick = {
                     // Lógica de que al dar en el botón, este se regrese uno en el backstack
-                }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()  // Solo ocupa el ancho
+                    .background(TopBarColor)
+                    .statusBarsPadding() // Para no sobreponer la TopBar con la de notificaciones
+                    .height(64.dp)
+                    .padding(horizontal = 8.dp)
             )
         }
     ) { innerPadding ->
@@ -83,27 +101,86 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 .padding(innerPadding)
                 .background(BackgroundApp)
         ) {
-            Text(
-                text = "Prueba",
-                fontFamily = FontFamily(Font(R.font.inter_regular)),
-                color = Black
-            )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(24.dp)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ){
+                // Espacio entre TopBar e imagen de perfil
+                Spacer(
+                    modifier = Modifier.size(30.dp)
+                )
+
+                // Composable que genera foto de perfil
+                FotoPerfil(
+                    modifier = Modifier
+                        .size(200.dp)
+                )
+
+                // Espacio entre imagen de perfil y título del formulario
+                Spacer(
+                    modifier = Modifier.size(30.dp)
+                )
+
+                // Título
+                Text(
+                    // !!!!!!!!!!! ========== HARD CODING ========= !!!!!!!!!!!
+                    text = "Creemos tu historia",
+                    fontFamily = FontFamily(Font(R.font.manrope_regular)),
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Black
+                )
+
+                // Mensaje introductorio
+                Text(
+                    // !!!!!!!!!!! ========== HARD CODING ========= !!!!!!!!!!!
+                    text = "Completa los siguientes campos para iniciar",
+                    fontFamily = FontFamily(Font(R.font.inter_regular)),
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Black,
+                    textAlign = TextAlign.Center                     // Centra el texto multilínea
+                )
+
+                // Espacio entre texto introductorio y sección 1
+                Spacer(
+                    modifier = Modifier.size(30.dp)
+                )
+
+                // ================== SECCIÓN 1. INFORMACIÓN PERSONAL ===============
+                /*
+                    * Pasando el Modifier como parámetro puedo cambiar ligeramente
+                    * el aspecto de este componente si decido reusarlo sin necesidad
+                    * de reescribir toda la función
+                    * */
+                TituloSeccion(
+                    // !!!!!!!!!!! ========== HARD CODING ========= !!!!!!!!!!!
+                    "Información personal",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+
+            }
+
         }
     }
 }
 
-// Función que se encarga de generar el composable de mi topBar
+// ============================= TOP BAR =============================
+// Función que se encarga de generar el Composable de mi TopBar
 @Composable
 fun TopBar(
     content: String,
-    onBackClick: () -> Unit
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
 ){
     Box(
-        modifier = Modifier
-            .fillMaxWidth()  // Solo ocupa el ancho
-            .height(64.dp)
-            .background(TopBarColor)
-            .padding(horizontal = 8.dp)
+        modifier = modifier
     ){
         IconButton(
             onClick = onBackClick,
@@ -113,7 +190,7 @@ fun TopBar(
                 imageVector = ImageVector.vectorResource(id = R.drawable.back_arrow),
                 // !!!!!!!!!!! ========== HARD CODING ========= !!!!!!!!!!!
                 contentDescription = "Atrás",
-                tint = Black
+                tint = DarkBlue
             )
         }
 
@@ -128,6 +205,78 @@ fun TopBar(
     }
 }
 
+// ============================= TÍTULOS DE SECCIONES DE FORMULARIO =============================
+@Composable
+fun TituloSeccion(texto: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // 1. Línea vertical azul
+        Box(
+            modifier = Modifier
+                .width(4.dp)       // Grosor de la línea
+                .height(24.dp)      // Altura de la línea
+                .clip(CircleShape)  // Orillas redondas
+                .background(DarkBlue)
+        )
+
+        // 2. Espacio entre la línea y el texto
+        Spacer(modifier = Modifier.width(12.dp))
+
+        // 3. El texto del título
+        Text(
+            text = texto,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Black
+        )
+    }
+}
+
+
+// ============================= FOTO DE PERFIL =============================
+@Composable
+fun FotoPerfil(modifier: Modifier = Modifier){
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        // 1. Imagen de perfil
+        Image(
+            painter = painterResource(id = R.drawable.foto_perfil),
+            // !!!!!!!!!!! ========== HARD CODING ========= !!!!!!!!!!!
+            contentDescription = "Foto de perfil",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+                .border(
+                    BorderStroke(2.dp, DarkBlue),
+                    CircleShape
+                )
+                .padding(2.dp)
+                .clip(CircleShape)
+        )
+
+        // 2. Ícono de cambiar imagen (Usar IconButton)
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .size(32.dp)
+                .background(color = DarkBlue, shape = CircleShape)
+                .align(Alignment.BottomEnd) // Alinea el botón en la esquina inferior derecha
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.cambiar_foto_lapiz),
+                // !!!!!!!!!!! ========== HARD CODING ========= !!!!!!!!!!!
+                contentDescription = "Cambiar foto de perfil",
+                tint = TopBarColor,
+                modifier = Modifier.size(20.dp)
+            )
+        }
+
+    }
+}
 
 
 // ======= Función necesaria para previsualizar la app dentro del IDE =======
